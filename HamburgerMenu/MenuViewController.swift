935c87345509cd2ad9f52fe0a6f31606c647eaac
Mary <mlmartinez85@gmail.com>
2017-01-07 10:07:16 -0800

    Setup application navigation and view controllers

--- HamburgerMenu/MenuViewController.swift
+++ HamburgerMenu/MenuViewController.swift
@@ -13,11 +13,18 @@ class MenuViewController: UIViewController {
     var hamburgerViewController: HamburgerViewController!
     
     var viewControllers = [UIViewController]()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // instantiate storyboards and navigation controllers
+        let firstStoryboard = UIStoryboard(name: "First", bundle: nil)
+        let secondstoryboard = UIStoryboard(name: "Second", bundle: nil)
+        let firstNavigationController = firstStoryboard.instantiateViewController(withIdentifier: "FirstNavigationController")
+        let secondNavigationController = secondstoryboard.instantiateViewController(withIdentifier: "SecondNavigationController")
+        
+        viewControllers = [firstNavigationController, secondNavigationController]
+        hamburgerViewController.contentViewController = firstNavigationController
     }
 
 }
