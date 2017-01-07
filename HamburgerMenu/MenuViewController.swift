e30407d601ad66ee7e5a9db04ec9ddfb66bd6c5a
Mary <mlmartinez85@gmail.com>
2017-01-07 07:40:19 -0800

    Add hamburger and menu view controllers

---
+++ HamburgerMenu/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  HamburgerMenu
+//
+//  Created by Mary Martinez on 1/7/17.
+//  Copyright © 2017 Mary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
