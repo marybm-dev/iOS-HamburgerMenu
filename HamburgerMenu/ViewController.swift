b2deb94adf87e6512da8c89038a52ab169d457ca
Mary <mlmartinez85@gmail.com>
2017-01-07 07:33:12 -0800

    Initial Commit

---
+++ HamburgerMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburgerMenu
+//
+//  Created by Mary Martinez on 1/7/17.
+//  Copyright © 2017 Mary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
